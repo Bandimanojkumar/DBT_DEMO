465365dede593456cc2c2d92e3fb4881fdd50fc9
ManojKumar Bandi <bandimanojkumar819@gmail.com>
2025-12-09 13:36:46 +0000

    station_dimension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,13 @@
+with BIKE as (
+    select 
+    start_statio_id as START_STATION_ID,
+    start_station_name as station_name,
+    start_lat as start_station_lat,
+    start_lng as start_station_lng
+    from {{ source('demo', 'bike') }}
+    where RIDE_ID!='ride_id'
+
+    limit 10
+)
+
+select * from BIKE
